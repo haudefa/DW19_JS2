bcc148980fe7ce7321e5529c0570c025feccb339
haudef me <meranthe.haudefa@gmail.com>
2022-06-27 14:26:21 +0200

    changement de variable

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.error("test")
+/*
+1.les variable 
+2. les condition 
+3.les boucle 
+4.les fonction 
+
+
+
+
+*/
+a=10 
+b=70
+c="salut"
+c=5
+console.error("a+b");
+console.error(c)
+SEL=100
+SUCRE=80
+console.error("Nous avons du SEL Q. de " +SEL +". Nous avons du sucre de Q. de " +SUCRE )
+SEL=80
+SUCRE=100
+
+console.error("Nous avons du SEL Q. de " +SEL +". Nous avons du sucre de Q. de " +SUCRE )
