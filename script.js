319bd2573871f2aa5e42c2060952f66905a984ed
haudef me <meranthe.haudefa@gmail.com>
2022-06-27 16:45:11 +0200

    prix de l'article

--- script.js
+++ script.js
@@ -17,9 +17,16 @@ console.error(c)
 SEL=100
 SUCRE=80
 console.error("Nous avons du SEL Q. de " +SEL +". Nous avons du sucre de Q. de " +SUCRE )
-TEMP=SEL
-SEL=SUCRE
-SUCRE=TEMP
-
-
+TEMP=SEL;
+SEL=SUCRE;
+SUCRE=TEMP;
 console.error("Nous avons du SEL Q. de " +SEL +". Nous avons du sucre de Q. de " +SUCRE )
+age = prompt("quel est ton age ?");
+B=age+2
+console.error("L'utilisateur à"+ age +"ans");
+console.error("L'utilisateur à"+ B +"ans");
+
+prix = prompt("quel est le prix de l'article?")
+console.error("l'article vaut " + prix +"euros")
+TTC=parseInt(prix)*1.20
+console.error("l'article vaut "+ TTC +"euros TTC")
