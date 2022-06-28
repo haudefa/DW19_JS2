514e008f264fc6913ff8e7dd1af068b765e90828
haudef me <meranthe.haudefa@gmail.com>
2022-06-28 14:48:30 +0200

    ajout d'une function

--- script2.js
+++ script2.js
@@ -56,7 +56,58 @@ for(a=0;a<11;a++){
 for (c=0;c<11;c++){
      console.log(c)
      for (d=0;d<11;d++){
-        console.log(c+ "+" +d+ " = " +(c+d))
+        console.log(c+ "+" + d + " = " + (c+d))
         
    }
 }
+
+g="*"
+for (h=0; h<101;h++){
+    console.log(g)
+    
+}
+
+for (h=0; h<101;h++){
+    console.log (g+g+g+g+g)
+    
+   
+}
+function fonctions(){
+    console.log("je test ma première fonction")
+}
+fonctions();
+function recup_age(){
+    age= prompt("Veuiller entré votre âge")
+    console.log(age)
+    age2=parseInt(age)+2
+    console.log(age2)
+
+}
+recup_age();
+function afficher(prenom){
+    console.log("bonjour" + prenom)
+}
+afficher("haudefa");
+afficher("larissa")
+afficher("gamal")
+
+function voir(numb){
+    numb = parseInt (numb)+2
+    console.log( numb)
+    if (numb>0){
+        console.log("ce nombre est positif")
+    }
+    else{
+        console.log("ce nombre est négatif")
+    }
+    if((numb %2)==0){
+        console.log("ce nombre est paire")
+    }
+    else{
+        console.log("ce nombre est impaire")
+    }
+}
+voir(5)
+voir(4)
+voir(-40)
+
