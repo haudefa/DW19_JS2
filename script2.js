75159f2d953436a239b94c6895d2036b1cb5ffee
haudef me <meranthe.haudefa@gmail.com>
2022-06-28 11:44:57 +0200

    ajout d'une boucle

--- script2.js
+++ script2.js
@@ -1,30 +1,48 @@
-age = prompt("Veuiller entré votre âge")
- age = parseInt(age)
- if (age>18){
-    console.error("vous êtes majeur")
- }
- if (age<18){
-    console.error("vous êtes mineur")
+// age = prompt("Veuiller entré votre âge")
+//  age = parseInt(age)
+//  if (age>18){
+//     console.error("vous êtes majeur")
+//  }
+//  if (age<18){
+//     console.error("vous êtes mineur")
 
- }
- if (age==18){
-    console.error("vous pille majeur")
- }
+//  }
+//  if (age==18){
+//     console.error("vous pille majeur")
+//  }
 
- nombre = prompt("veuillez entré un nombre")
- nombre = parseInt(nombre)
- if (nombre>0){
-    console.error("le nombre est positif")
- }
- if (nombre<0){
-    console.error("le nombre est négatif")
- }
+//  nombre = prompt("veuillez entré un nombre")
+//  nombre = parseInt(nombre)
+//  if (nombre>0){
+//     console.error("le nombre est positif")
+//  }
+//  if (nombre<0){
+//     console.error("le nombre est négatif")
+//  }
 
-number = prompt("Veuiller entré un nombre")
-number = parseInt(number)
-if ((number % 2) ==0){
-    console.error("le nombre est paire")
+// number = prompt("Veuiller entré un nombre")
+// number = parseInt(number)
+// if ((number % 2) ==0){
+//     console.error("le nombre est paire")
+// }
+// else{
+//     console.error("le nombre est impaire")
+// }
+prenom = "haudefa"
+nom = prompt("veuiller entrér votre prénom")
+if (prenom==nom){
+    console.error("sympa on a le mêmê prénom")
 }
 else{
-    console.error("le nombre est impaire")
+    console.error("")
+}
+// for (i=0;i<5;i++){
+//     alert("Sympa une boucle")
+// }
+
+for (i=1;i<100;i++){
+    console.error(i)
+}
+for (y=100;y<200;y++){
+    console.error(y)
 }
